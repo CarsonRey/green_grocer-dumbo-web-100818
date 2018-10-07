e92b66fdaf215f474b238252c81feed66968adcc
Carson Rey <c.rey494@gmail.com>
2018-10-07 18:12:58 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -22,8 +22,7 @@ def apply_coupons(cart, coupons)
  cart.each do |item, item_hash|
   coupons.each do |coupon_hash|
     coupon_hash.each do |key, value|
-      if item == coupon_hash[:item] && cart[item][:count] >= coupon_hash[:num]
-        cart[item][:count] -= coupon_hash[:num]
+      if item == coupon_hash[:item] 
           
      else
         new_cart[item] = item_hash
