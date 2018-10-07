4ff5345eadd8386b8fbce7fde683546eb5199637
Carson Rey <c.rey494@gmail.com>
2018-10-07 19:06:36 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -15,19 +15,31 @@ require 'pry'
   hash
 end
 
- [
-      {:item => "AVOCADO", :num => 2, :cost => 5.00},
-      {:item => "BEER", :num => 2, :cost => 20.00},
-      {:item => "CHEESE", :num => 3, :cost => 15.00}
-    ]
+ # [
+ #     {:item => "AVOCADO", :num => 2, :cost => 5.00},
+ #     {:item => "BEER", :num => 2, :cost => 20.00},
+  #    {:item => "CHEESE", :num => 3, :cost => 15.00}
+  #  ]
 
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]  
+        cart["#{name} W/COUPON"][:count] += 1
+        
+      else
+      cart["#{name} W/COUPON"] = {
+        :count => 1
+        :price => coupon[:cost]
+      }
+      cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+      end
+    else
+      cart[name][:count]
+    end
   end
-
-
 end
 
 
