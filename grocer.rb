df8199a1e029ea54ed38ad9b4a55d61b071b4b8c
Carson Rey <c.rey494@gmail.com>
2018-10-07 19:46:52 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -57,7 +57,7 @@ def apply_clearance(cart)
   cart
 end
 
-def checkout(cart:[], coupons: [])
+def checkout(cart: , coupons: )
      cart = consolidate_cart(cart)
      cart = apply_coupons(cart, coupons)
      cart = apply_clearance(cart)
