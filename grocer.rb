3568a4ac186e6ca1bccf5d4407dda10b72fb6b15
Carson Rey <c.rey494@gmail.com>
2018-10-07 18:38:00 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -24,13 +24,13 @@ def apply_coupons(cart, coupons)
     coupon_hash.each do |key, value|
      if item == coupon_hash[:item]
       new_cart["#{item} W/COUPON"] = {
-                  :price => coupon_hash[:cost],
-                  :clearance => true,
-                  :count => cart[item][:count] / coupon_hash[:num] }
+         :price => coupon_hash[:cost],
+         :clearance => true,
+         :count => cart[item][:count] / coupon_hash[:num] }
       new_cart[item] = {
-                  :price => cart[item][:price],
-                  :clearance => true, 
-                  :count => cart[item][:count] - coupon_hash[:num]}     
+         :price => cart[item][:price],
+         :clearance => true, 
+         :count => cart[item][:count] - coupon_hash[:num]}     
      else
         new_cart[item] = item_hash
       end
