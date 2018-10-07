4f1ec195ca0f93836eb51dc7b954204422e76adf
Carson Rey <c.rey494@gmail.com>
2018-10-07 20:17:09 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -61,7 +61,12 @@ def checkout(cart:[], coupons:[])
      cart = consolidate_cart(cart: cart)
      cart = apply_coupons(cart: cart, coupons: coupons)
      cart = apply_clearance(cart: cart)
-    
+      total = array_of_prices.inject(0) {|result, price| result + price}
+      if total > 100
+        total * 0.90.round
+      else
+      total
+      end
     
     cart  
 end
